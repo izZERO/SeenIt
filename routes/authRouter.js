15d6f0fc9f9113194963682f48536832c5cfb0df
izZERO <crusherh7x@hotmail.com>
2025-10-02 15:50:29 +0300

    refactored auth controller / routes

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,12 +2,9 @@ const router = require('express').Router()
 const authCtrl = require('../controllers/authController')
 
 //API's
-router.get('/sign-up', (req, res) => {
-  res.render('./auth/sign-up.ejs')
-})
-router.post('/sign-up', authCtrl.registerUser)
-router.post('/sign-in', authCtrl.signInUser)
-router.get('/sign-in', (req, res) => {
-  res.render('./auth/sign-in.ejs')
-})
+router.get("/sign-up", authCtrl.auth_signup_get)
+router.post('/sign-up', authCtrl.auth_signup_post)
+router.get("/sign-in", authCtrl.auth_signin_get)
+router.post('/sign-in', authCtrl.auth_signin_post)
+
 module.exports = router
