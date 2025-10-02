47127428b74b357e9985600f25aeedb17f517e41
envwx <naz66cod@gmail.com>
2025-10-02 13:43:22 +0300

    fixed sign in

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,16 +1,16 @@
-const bcrypt = require("bcrypt")
-const User = require("../models/User.js")
+const bcrypt = require('bcrypt')
+const User = require('../models/User.js')
 
 const registerUser = async (req, res) => {
   try {
     const userInDatabase = await User.exists({ email: req.body.email })
 
     if (userInDatabase) {
-      return res.send("Username already taken!")
+      return res.send('Username already taken!')
     }
 
     if (req.body.password !== req.body.confirmPassword) {
-      return res.send("Password and Confirm password Must match")
+      return res.send('Password and Confirm password Must match')
     }
 
     const hashedPassword = bcrypt.hashSync(req.body.password, 10)
@@ -19,14 +19,14 @@ const registerUser = async (req, res) => {
       email: req.body.email,
       username: req.body.username,
       password: hashedPassword,
-      profilePicture: "../images/Profile/pfp.svg",
-      coverPicture: "../images/Profile/cover.jpg",
+      profilePicture: '../images/Profile/pfp.svg',
+      coverPicture: '../images/Profile/cover.jpg',
       following: [],
-      followers: [],
+      followers: []
     })
-    res.send("Thanks for signing up!")
+    res.send('Thanks for signing up!')
   } catch (error) {
-    console.error("An error has occurred registering a user!", error.message)
+    console.error('An error has occurred registering a user!', error.message)
   }
 }
 
@@ -44,7 +44,7 @@ const signInUser = async (req, res) => {
       email: user.email,
       _id: user._id
     }
-    res.redirect(`/users/${user._id}!`)
+    res.send(`Thanks for signing in, ${user.username}!`)
   } catch (error) {
     console.error('An error has occurred signing in a user!', error.message)
   }
