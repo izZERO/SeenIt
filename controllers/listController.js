52122d2039306b711f0f994d907d70fc91b8f0de
izZERO <crusherh7x@hotmail.com>
2025-10-05 16:14:10 +0300

    added add to watchlist, incomplete remove from watch list

--- controllers/listController.js
+++ controllers/listController.js
@@ -1,12 +1,25 @@
-const User = require("../models/User")
 const List = require("../models/List")
 
-
-exports.watchList_index_get = async (req,res) => {
-  const currentUser = await User.findById(req.session.user._id)
+exports.watchList_index_get = async (req, res) => {
+  const watchList = await List.findOne({
+    user: "req.session.user._id",
+    isWatchList: true,
+  })
   res.send(watchList)
 }
 
-exports.watchList_add_post = async (req,res) => {
-  const currentUser = await User.findById(req.session.user._id)
+exports.watchList_add_put = async (req, res) => {
+  await List.findOneAndUpdate(
+    { user: req.session.user._id, isWatchList: true },
+    req.body
+  )
+  res.send("List Updated")
+}
+
+exports.watchList_delete_delete = async (req, res) => {
+  await List.findOneAndUpdate(
+    { user: req.session.user._id, isWatchList: true },
+    { $pull: { movie: req.body.movieId } }
+  )
+  res.send("Movie removed from watchlist")
 }
--- routes/listRouter.js
+++ routes/listRouter.js
@@ -3,5 +3,6 @@ const listCtrl = require('../controllers/listController')
 
 //API's
 router.get("/watchlist", listCtrl.watchList_index_get)
+router.put("/watchlist", listCtrl.watchList_add_put)
 
 module.exports = router
